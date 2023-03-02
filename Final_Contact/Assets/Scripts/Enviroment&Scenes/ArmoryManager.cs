b908c31fbe9d8100502ce54be154597ad0fc3465
Love <117894224+GLoveDanger@users.noreply.github.com>
2023-03-02 13:16:57 +0100

    Armory dummy and pickups

--- Final_Contact/Assets/Scripts/Enviroment&Scenes/ArmoryManager.cs
+++ Final_Contact/Assets/Scripts/Enviroment&Scenes/ArmoryManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ArmoryManager : MonoBehaviour
 {
-    private GameObject[] pickupHealth;
-    private GameObject[] pickupFiringSpeed;
-    private GameObject[] dummy;
+    private float pickupHealth;
+    private float pickupFiringSpeed;
+    private float dummy;
     [SerializeField]
     private GameObject healthPrefab;
     [SerializeField]
@@ -15,28 +15,52 @@ public class ArmoryManager : MonoBehaviour
     private GameObject dummyPrefab;
     [SerializeField]
     private Transform[] pickupSpawnPoints;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        pickupHealth = GameObject.FindGameObjectsWithTag("PickupHealth");
-        pickupFiringSpeed = GameObject.FindGameObjectsWithTag("PickupFiringSpeed");
-        dummy = GameObject.FindGameObjectsWithTag("Enemy");
+        Instantiate(dummyPrefab, new Vector3(40,2.5f,0), Quaternion.Euler(0,270,0));
+        Instantiate(healthPrefab, pickupSpawnPoints[1].transform.position, Quaternion.identity);
+        Instantiate(fireRatePrefab, pickupSpawnPoints[0].transform.position, Quaternion.identity);    
     }
-
     // Update is called once per frame
     void Update()
     {
-        if(pickupHealth.Length < 1)
+        pickupHealth = GameObject.FindGameObjectsWithTag("PickupHealth").Length;
+        pickupFiringSpeed = GameObject.FindGameObjectsWithTag("PickupFiringSpeed").Length;
+        dummy = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if(pickupHealth < 1)
         {
+            RespawnHealthPickup();
+        }
+        if (pickupFiringSpeed < 1)
+        {
+            RespawnFirePickup();
+        }
+        if (dummy  < 1)
+        {
+            RespawnDummy();
+        }
+    }
+    private void RespawnHealthPickup()
+    {
+        float timeSinceDeath;
+        timeSinceDeath = Time.time;
+        if (Time.time > timeSinceDeath + 5)
             Instantiate(healthPrefab, pickupSpawnPoints[1].transform.position, Quaternion.identity);
-        }
-        if (pickupFiringSpeed.Length < 1)
-        {
-            Instantiate(healthPrefab, pickupSpawnPoints[0].transform.position, Quaternion.identity);
-        }
-        if (dummy.Length < 1)
-        {
-            Instantiate(dummyPrefab, new Vector3(40,2.5f,0), Quaternion.identity);
-        }
+            
+    }
+    private void RespawnFirePickup()
+    {
+        float timeSinceDeath;
+        timeSinceDeath = Time.time;
+        if (Time.time > timeSinceDeath + 5)
+            Instantiate(fireRatePrefab, pickupSpawnPoints[0].transform.position, Quaternion.identity);
+            
+    }
+    private void RespawnDummy()
+    {
+        float timeSinceDeath;
+        timeSinceDeath = Time.time;
+        if(Time.time > timeSinceDeath + 5)
+            Instantiate(dummyPrefab, new Vector3(40,2.5f,0), Quaternion.Euler(0,270,0));
     }
 }
