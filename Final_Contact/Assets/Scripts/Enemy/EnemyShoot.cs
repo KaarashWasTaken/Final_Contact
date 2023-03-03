53c938396b6b715ea92b2fb92d2e16796a4ee06b
Sebemblad <Sebemblad@gmail.com>
2023-03-03 15:24:30 +0100

    Gattling Enemy
    gattling enemy with bullet spread added

--- Final_Contact/Assets/Scripts/Enemy/EnemyShoot.cs
+++ Final_Contact/Assets/Scripts/Enemy/EnemyShoot.cs
@@ -7,17 +7,40 @@ public class EnemyShoot : MonoBehaviour
 
     [SerializeField]
     private Transform FiringPoint;
+    private Transform FiringPointDeviation;
     [SerializeField]
     private Rigidbody projectilePrefab;
     [SerializeField]
     private float firingspeed = 1f;
     private float lastTimeShot = 0;
+    [SerializeField]
+    private float shootSpread = 15;
+    private Quaternion originalAngle;
+
+
+    private void Start()
+    {
+        FiringPointDeviation = FiringPoint;
+    }
     public void Shoot()
     {
         if (lastTimeShot + firingspeed <= Time.time)
         {
             lastTimeShot = Time.time;
             Instantiate(projectilePrefab, FiringPoint.position, FiringPoint.rotation);
+        }
+    }
+
+    public void SpreadShoot()
+    {
+        if (lastTimeShot + firingspeed <= Time.time)
+        {
+            originalAngle = FiringPoint.rotation;
+            lastTimeShot = Time.time;
+            FiringPoint.Rotate(0, 0, Random.Range(-shootSpread, shootSpread));
+            Instantiate(projectilePrefab, FiringPoint.position, FiringPoint.rotation);
+            FiringPoint.rotation = originalAngle;
+
         }
     }
 }
