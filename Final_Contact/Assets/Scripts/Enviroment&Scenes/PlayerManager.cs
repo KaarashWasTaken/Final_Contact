50701811d76d65415f927d0f20d88ff82b2ffafc
Love <117894224+GLoveDanger@users.noreply.github.com>
2023-03-17 15:30:40 +0100

    Increased ranged enemies range

--- Final_Contact/Assets/Scripts/Enviroment&Scenes/PlayerManager.cs
+++ Final_Contact/Assets/Scripts/Enviroment&Scenes/PlayerManager.cs
@@ -7,8 +7,6 @@ public class PlayerManager : MonoBehaviour
     PlayerInputManager playerManager;
     private GameObject[] players;
     private GameObject[] downedPlayers;
-    private float nrOfPlayers;
-    private GameObject gameOver;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -23,14 +21,12 @@ public class PlayerManager : MonoBehaviour
     // called second
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        gameOver = GameObject.FindWithTag("GameOver");
         Debug.Log("OnSceneLoaded: " + scene.name);
         scene = SceneManager.GetActiveScene();
-        checkSceneName(scene);
-        //Diables joining when the scene is not armory
-        //if (scene.name != "Armory")
+        //Disables joining when the scene is not armory
+        //if (scene.name != "LVLArmory")
         //    playerManager.DisableJoining();
-        //if (scene.name == "Armory")
+        //if (scene.name == "LVLArmory")
         //    playerManager.EnableJoining();
         players = GameObject.FindGameObjectsWithTag("Player");
         //Loops through the players and set them to unready
@@ -51,52 +47,7 @@ public class PlayerManager : MonoBehaviour
         if(downedPlayers.Length <= 0)
         {
             Debug.Log("GameOver");
-            //insert Gameover script here
             GameObject.FindWithTag("GameOver").GetComponent<GameOver>().Activate();
         }
-    }
-    private void checkSceneName(Scene scene)
-    {
-
-    }
-    private void LVL1A()
-    {
-
-    }
-    private void LVL1B()
-    {
-
-    }
-    private void LVL2A()
-    {
-
-    }
-    private void LVL2B()
-    {
-
-    }
-    private void LVL3()
-    {
-
-    }
-    private void LVL4A()
-    {
-
-    }
-    private void LVL4B()
-    {
-
-    }
-    private void LVL5A()
-    {
-
-    }
-    private void LVL5B()
-    {
-
-    }
-    private void LVL6()
-    {
-
     }
 }
