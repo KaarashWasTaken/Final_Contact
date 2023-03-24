6faf7f6954114aec8362c360a79c1a087853cd95
Aron <aron.olovsson@gmail.com>
2023-03-24 10:29:32 +0100

    Pause menu and controls in main, also spawn popup

---
+++ Final_Contact/Assets/Scripts/Enviroment&Scenes/Menus/SpawnPopup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPopup : MonoBehaviour
+{
+    int nrOfPlayers;
+    public GameObject Popup;
+    void Update()
+    {
+        //tells the player to press a to spawn if noone has spawned
+        nrOfPlayers = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (nrOfPlayers != 0)
+        {
+            Popup.SetActive(false);
+        }
+    }
+}
